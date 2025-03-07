b10656d6dc1570b7495bf5bb169523dd26522053
macricardo <300877933@pcpuma.acatlan.unam.mx>
2025-03-06 20:38:24 -0600

    premier

---
+++ src/test/java/primer/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package primer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
